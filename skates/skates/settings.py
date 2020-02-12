214f3564c58c253eb16ad83ae2340fae2f2178cb
michaelmurwayi <https://github.com/michaelmurwayi>
2020-02-12 09:51:09 +0300

    configuration of static files

--- skates/skates/settings.py
+++ skates/skates/settings.py
@@ -114,8 +114,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# import ipdb; ipdb.set_trace()
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hire/static'),
+]
