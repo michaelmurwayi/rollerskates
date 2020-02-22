da89a66d8192bb1153555b1fa222919ecfec48a2
michaelmurwayi <https://github.com/michaelmurwayi>
2020-02-22 18:09:55 +0300

    configuration of navigation links, signup and login page creation

--- carwash/management/urls.py
+++ carwash/management/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.urls import path, include 
 from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name="home"),
     path('signup/', views.SignUpView.as_view(), name="signup"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
