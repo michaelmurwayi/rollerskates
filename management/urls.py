e5985150854f06e53f122812a45bfb9549a5c72f
michaelmurwayi <https://github.com/michaelmurwayi>
2020-03-22 23:13:49 +0300

    handling reservation for carwash services

---
+++ management/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include 
+from . import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('signup/', views.SignUpView.as_view(), name="signup"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+]
---
+++ management/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from djreservation.views import ProductReservationView
+from .models import Reservation
+
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+    
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accounts/')
+    template_name = "signup.html"
+
+    def post(self, request):
+        form = self.form_class(request.post)
+        
+        if form.is_valid():
+            return HttpResponseRedirect('/admin/')
+
+            return render(request, self.template_name, {'form': form})
