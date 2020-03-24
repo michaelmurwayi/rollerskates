ddb6ee1f5476b62cfaf031bbb59e26eada2246d9
michaelmurwayi <https://github.com/michaelmurwayi>
2020-03-24 23:06:47 +0300

    resolving merge conflicts

--- management/urls.py
+++ management/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include 
 from . import views
-<<<<<<< HEAD
 from .views import CarWashReservation
-=======
->>>>>>> d3bfcdbf8821f64d0e69c97ba88df0a45d59d1ff
 from django.views.generic import TemplateView
 
 
@@ -11,9 +8,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
     path('signup/', views.SignUpView.as_view(), name="signup"),
     path('accounts/', include('django.contrib.auth.urls')),
-<<<<<<< HEAD
     path('profile/', TemplateView.as_view(template_name="profile.html"), name="profile"),
-=======
->>>>>>> d3bfcdbf8821f64d0e69c97ba88df0a45d59d1ff
     
 ]
