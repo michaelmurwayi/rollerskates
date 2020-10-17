8a2f98e8e8af21b9fc11643caf580a73c3822f92
michaelmurwayi <39840500+michaelmurwayi@users.noreply.github.com>
2020-10-17 15:07:42 +0300

    Update views.py

--- hire/views.py
+++ hire/views.py
@@ -10,7 +10,7 @@ class HomeView(TemplateView):
     
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('accounts/')
+    success_url = reverse_lazy('profile/')
     template_name = "signup.html"
 
 
